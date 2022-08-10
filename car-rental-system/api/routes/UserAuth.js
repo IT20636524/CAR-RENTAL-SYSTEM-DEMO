90f659d5ae557925b565c5470d842067cb40ecc5
PrathibhaSathyanjalee <88728787+IT20636524@users.noreply.github.com>
2022-08-10 23:55:11 +0530

    Initial commit

---
+++ car-rental-system/api/routes/UserAuth.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//Register
+router.post("/register", async (req,res) => {
+    
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+        user_id : req.body.user_id,
+        name : req.body.name,
+        email : req.body.email,
+        password : hashedPass,
+        profile_pic : req.body.profile_pic,
+    });
+    try{
+        const usercount = await User.count()
+        newUser.user_id = 'U00' + (parseInt(usercount)+1)
+        try{
+            const user = await newUser.save();
+            res.status(200).json(user);
+        } catch (err){
+            res.status(500).json(err);
+        }
+    }catch(err){
+        console.log(err);
+    }
+});
+
+//Login
+router.post("/login", async(req,res) => {
+    try{
+        const user = await User.findOne({ email : req.body.email})
+        !user && res.status(400).json("Wrong credentials")
+
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong credentials")
+
+        const { password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Get users
+router.get("/:user_id", async(req,res) => {
+    try{
+        const user = await User.findOne({ 'user_id':req.params.user_id });
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
---
+++ car-rental-system/api/routes/bookings.js
@@ -0,0 +1,71 @@
+const router =require("express").Router();
+const Booking = require("../models/Booking");
+
+//Add booking
+router.post("/add", async (req,res) => {
+    const newBooking = new Booking(req.body);
+    try{
+        const bookingcount = await Booking.count()
+        newBooking.booking_id = 'B00' + (parseInt(bookingcount)+1)
+        try{
+            const booking = await newBooking.save();
+            res.status(200).json(booking);
+        } catch (err){
+            res.status(500).json(err);
+        }
+    }catch(err){
+        console.log(err);
+    }
+});
+
+//Get all bookings
+router.get("/", async(req,res) => {
+    const book_id = req.query.booking_id;
+    try{
+        let bookings;
+        if(book_id) {
+            bookings = await Booking.find({ booking_id });
+        } else {
+            bookings = await Booking.find();
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Get one booking
+router.get("/:booking_id", async(req,res) => {
+    try{
+        const booking = await Booking.findOne({ 'booking_id':req.params.booking_id });
+        res.status(200).json(booking);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Update booking
+router.put("/update/:id", async(req,res) => {
+    try{
+        const updatedBooking = await Booking.findOneAndUpdate({'booking_id':req.params.id},
+            {
+                $set:req.body
+            },{new:true}
+        );
+        res.status(200).json(updatedBooking);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Delete booking
+router.delete("/delete/:id", async(req,res) => {
+    try{
+        const deletedBooking = await Booking.findOneAndDelete({'booking_id':req.params.id});
+        res.status(200).json("Booking has been deleted");
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+module.exports = router;
